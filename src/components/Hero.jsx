1e8f2cf44327a9991ca281093977efd999606881
Viachaslau Rouski <vrovsky@gmail.com>
2025-05-13 17:13:19 +0100

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,26 @@
+export default function Hero() {
+  return (
+    <div className="min-h-screen text-center flex flex-col gap-10 justify-center items-center max-w-[900px] w-full mx-auto p-4">
+      <div className="flex flex-col gap-4 justify-center items-center">
+        <p>IT'S TIME TO GET</p>
+        <h1 className="uppercase font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl">
+          Swole<span className="text-blue-400">Fit</span>
+        </h1>
+      </div>
+      <p className=" text-sm md:text-base font-light">
+        I hereby acknowledgement that I may become{" "}
+        <span className="text-blue-400 font-medium">
+          unbelievably swolenormous{" "}
+        </span>{" "}
+        and accept all risks of becoming the local{" "}
+        <span className="text-blue-400 font-medium">
+          superheromass montrosity
+        </span>
+        , afflicted with severe body dismorphia, unable to fit through doors.
+      </p>
+      <button className="px-8 py-4 rounded-md border-[2px] bg-slate-950 border-blue-400 border-solid blueShadow duration-200">
+        <p>Accept & Begin</p>
+      </button>
+    </div>
+  );
+}
